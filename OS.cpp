facb8f2f0909b35c8b974537c4d23b7482dd6766
neshatsh <ne.sharbatdar@gmail.com>
2024-10-06 19:29:24 -0400

    cleaned code

--- OS.cpp
+++ OS.cpp
@@ -8,36 +8,39 @@
 
 int main()
 {
-   const std::size_t total_points_arg = 10000000;
-   static constexpr std::size_t ThreadsNum = 4;
+   const int total_points_arg = 10000000;
+   constexpr int threads_num = 4;
 
    std::atomic<size_t> total_points_in(0);
-   const auto run = [&total_points_in](const std::size_t points){
-      std::size_t points_in = 0;
+   const auto run = [&total_points_in](const int points){
+      int points_in = 0;
       std::random_device random_device;
       std::mt19937 gen(random_device());
       std::uniform_real_distribution<> dis(0.0, 1.0);
 
-      for (std::size_t i = 0; i < points; i++)
+      for (int i = 0; i < points; i++)
       {
          const double x_value = dis(gen);
          const double y_value = dis(gen);
          const double distance = std::sqrt((x_value * x_value) + (y_value * y_value));
 
-         if (distance <= 1)
+         if (distance <= 1) {
             points_in++;
+         }
       }
       total_points_in.fetch_add(points_in, std::memory_order_relaxed);
    };
 
    const auto start = std::chrono::high_resolution_clock::now();
 
-   std::thread threads[ThreadsNum];
-   for (std::size_t i = 0; i < ThreadsNum; i++)
-      threads[i] = std::thread(run, total_points_arg / ThreadsNum);
+   std::thread threads[threads_num];
+   for (auto &thread : threads) {
+      thread = std::thread(run, total_points_arg / threads_num);
+   }
 
-   for (std::size_t i = 0; i < ThreadsNum; i++)
-      threads[i].join();
+   for (auto &thread : threads) {
+      thread.join();
+   }
 
    const auto end = std::chrono::high_resolution_clock::now();
    std::chrono::duration<double> duration = end - start;
